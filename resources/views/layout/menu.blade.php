bc493ede6313cc95eb470a28f4b985c05d162525
unknown <dwiantoroarko@students.unnes.ac.id>
2023-05-21 20:59:54 +0700

    first commit

---
+++ resources/views/layout/menu.blade.php
@@ -0,0 +1,52 @@
+<nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="{{ url('layout/home') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="http://127.0.0.1:8000/arsip" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Permohonan Cuti
+                <i class="right fas fa-angle-right"></i>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="http://127.0.0.1:8000/arsip" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Gaji
+                <i class="right fas fa-angle-right"></i>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="http://127.0.0.1:8000/arsip" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Absensi
+                <i class="right fas fa-angle-right"></i>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="http://127.0.0.1:8000/arsip" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Absensi Harian
+                <i class="right fas fa-angle-right"></i>
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
